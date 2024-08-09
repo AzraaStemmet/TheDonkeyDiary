fcc4e2a104566aa7024b7a80a97ee663581c186c
AzraaStemmet <STMAZR001@myuct.ac.za>
2024-08-09 22:21:46 +0200

    fb db

---
+++ HomeScreen.js
@@ -0,0 +1,43 @@
+// screens/HomeScreen.js
+import React from 'react';
+import { StyleSheet, View, Text, Image, Button } from 'react-native';
+
+const HomeScreen = ({ navigation }) => {
+  return (
+    <View style={styles.container}>
+      <Image source={require('./assets/icon.jpg')} style={styles.image} />
+      <Text style={styles.title}>Welcome to The Donkey Diary</Text>
+      <Text style={styles.description}>
+        Your trusted platform for managing donkey health and information in rural villages.
+      </Text>
+      <Button title="Login" onPress={() => navigation.navigate('Login')} />
+    </View>
+  );
+};
+
+export default HomeScreen;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#f5f5dc',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  image: {
+    width: 300,
+    height: 200,
+    marginBottom: 20,
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    marginBottom: 10,
+  },
+  description: {
+    fontSize: 16,
+    textAlign: 'center',
+    marginHorizontal: 20,
+    marginBottom: 20,
+  },
+});
---
+++ firebaseConfig.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCKHWXBJ26ut-kectGIyCKcPy90tdDNbYM",
+  authDomain: "thedonkeydiary.firebaseapp.com",
+  projectId: "thedonkeydiary",
+  storageBucket: "thedonkeydiary.appspot.com",
+  messagingSenderId: "360396801033",
+  appId: "1:360396801033:web:7d85020462e389f77be4d8"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
