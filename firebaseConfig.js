8e86925efe1c86831cde53c6d17c63d5ae0c6bf3
AzraaStemmet <STMAZR001@myuct.ac.za>
2024-08-11 17:58:04 +0200

    comments on firebase config

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -1,17 +1,18 @@
-// Import the functions you need from the SDKs you need
+//  is a setup script to integrate Firebase into the application
+// essential to initialize and configure our Firebase app
 import { initializeApp } from "firebase/app";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyCKHWXBJ26ut-kectGIyCKcPy90tdDNbYM",
-  authDomain: "thedonkeydiary.firebaseapp.com",
-  projectId: "thedonkeydiary",
-  storageBucket: "thedonkeydiary.appspot.com",
-  messagingSenderId: "360396801033",
-  appId: "1:360396801033:web:7d85020462e389f77be4d8"
+  apiKey: "AIzaSyCKHWXBJ26ut-kectGIyCKcPy90tdDNbYM", // used for authenticating requests from the app
+  authDomain: "thedonkeydiary.firebaseapp.com", // used for hosting authentication UI and handling auth-related requests
+  projectId: "thedonkeydiary", // unique identifier for firebase application
+  storageBucket: "thedonkeydiary.appspot.com", // used for storing images and documents
+  messagingSenderId: "360396801033", // handles sending notifications
+  appId: "1:360396801033:web:7d85020462e389f77be4d8" // unique identifier for firebase app instance
 };
 
-// Initialize Firebase
+// Initialize Firebase, used to interact with various firebase services
 const app = initializeApp(firebaseConfig);
