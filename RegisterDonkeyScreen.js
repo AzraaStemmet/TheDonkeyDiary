ad36b1cb24da08c3f4ecbcd35ab41d6e87540bd6
AzraaStemmet <STMAZR001@myuct.ac.za>
2024-09-06 16:14:34 +0200

    updated

--- RegisterDonkeyScreen.js
+++ RegisterDonkeyScreen.js
@@ -205,14 +205,14 @@ const RegisterDonkeyScreen = () => {
               generateUniqueId();
             }}
             items={[
-              { label: '< 12 months', value: '< 12 months' },
-              { label: '1 year', value: '1 year' },
-              { label: '2 years', value: '2 years' },
-              { label: '3 years', value: '3 years' },
-              { label: '4 years', value: '4 years' },
-              { label: '5 years', value: '5 years' },
-              { label: '6 years', value: '6 years' },
-              { label: '> 7 years', value: '> 7 years' },
+              { label: '< 12 months', value: 12 },
+              { label: '1 year', value: 1 },
+              { label: '2 years', value: 2 },
+              { label: '3 years', value: 3  },
+              { label: '4 years', value: 4 },
+              { label: '5 years', value: 5 },
+              { label: '6 years', value: 6 },
+              { label: '> 7 years', value: 7 },
             ]}
             style={pickerSelectStyles}
             value={age}
